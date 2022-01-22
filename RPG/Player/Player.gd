ba040bdbbe32870267f7556b3457d2c3a96b2a7d
Egarozz <68447250+Egarozz@users.noreply.github.com>
2022-01-22 13:57:00 -0500

    Second

--- RPG/Player/Player.gd
+++ RPG/Player/Player.gd
@@ -6,9 +6,13 @@ const friction = 400
 
 var velocity = Vector2.ZERO
 var input_vector = Vector2.ZERO
+
+var animation_player = null
+onready var animation_tree = $AnimationTree
+onready var animation_state = animation_tree.get("parameters/playback")
+
 func _ready():
-	
-	pass # Replace with function body.
+	animation_player = $AnimationPlayer
 
 func _physics_process(delta):
 	input_vector = Vector2.ZERO
@@ -16,10 +20,15 @@ func _physics_process(delta):
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up") 
 	input_vector = input_vector.normalized()
 	if input_vector != Vector2.ZERO:
+		animation_tree.set("parameters/Idle/blend_position", input_vector)
+		animation_tree.set("parameters/Run/blend_position", input_vector)
+		animation_state.travel("Run")
 		velocity += input_vector * acceleration * delta
 		velocity = velocity.clamped(max_speed)
 		print(velocity)
 	else:
+		animation_state.travel("Idle")
+		
 		velocity = velocity.move_toward(Vector2.ZERO, friction*delta)
 	
 	velocity = move_and_slide(velocity)
